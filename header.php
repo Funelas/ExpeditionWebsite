c001c3e50fdc9ae8652cb0331c18dfc2cd7c1000
Allan John Funelas <funelasallanjohn@gmail.com>
2025-04-03 17:20:15 +0800

    Done main/index.php design

---
+++ header.php
@@ -0,0 +1,4 @@
+<a href="index.php" class="mb-3 text-[#0d0f0f] text-4xl md:text-5xl lg:text-7xl px-4 py-2 flex flex-col justify-center items-center [font-family:'Orbitron']">
+    <img class="w-[40px] h-[40px] md:w-[50px] md:h-[50px] lg:w-[70px] lg:h-[70px]" src="https://img.icons8.com/ios-filled/100/mission-of-a-company.png" alt="mission-of-a-company"/>
+    Expedition
+    </a>
--- index.php
+++ index.php
@@ -33,33 +33,43 @@
                             echo "<script>window.location.href='user'</script>";
                         }
                     }else{
-                        $passwordErr = "Password is incorrect!";
+                        $passwordErr = "Account not found.";
                     }
                 }
             } else{
-                $emailErr = "Email is not registered!";
+                $emailErr = "Account not found.";
             }
         }
     }
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class= "h-full">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Expedition</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&display=swap">
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Lexend+Deca&family=Orbitron:wght@400..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-    <form action="<?php echo $_SERVER['PHP_SELF']?>" method= "POST">
-        <h3>Email:</h3>
-        <input type="text" name = "email" value="<?php echo $email;?>">
-        <span><?php echo $emailErr?></span>
-        <h3>Password:</h3>
-        <input type="password" name = "password" value="<?php echo $password; ?>">
-        <span><?php echo $passwordErr?></span><br>
-        <input type="submit">
-    </form>
-    <span>Still not registered?<a href="register.php">Click Here!</a></span>
+<body class= "[font-family:'Kanit'] bg-[#dfe0dc] w-full h-full flex flex-col lg:flex-row lg:justify-around lg:items-around justify-center items-center text-[#dfe0dc]">
+    <?php include ('header.php');?>
+    <div class="w-full md:w-[50%] px-4 py-2 flex flex-col justify-center items-center">
+        <img class="w-[96px] w-[96px]" src="https://img.icons8.com/fluency-systems-regular/96/user-male-circle--v1.png" alt="user-male-circle--v1"/>
+        <h3 class="text-[#0d0f0f] text-3xl">Member Login</h3>
+        <form action="<?php echo $_SERVER['PHP_SELF']?>" method= "POST" class= "px-4 py-2 px-4 py-2 bg-[#0d0f0f] rounded-3xl border-4 shadow flex flex-col justify-center items-center md:w-[50%] lg:w-[50%]">
+            <div class="flex flex-end w-full mx-4 my-2 text-xl"><h3>Email:</h3></div>
+            <input class= "w-full rounded-full px-4 py-2 text-[#0d0f0f] text-xl" type="text" name = "email" value="<?php echo $email;?>">
+            <span class= 'text-[#dc3545]'><?php echo $emailErr?></span>
+            <div class="flex flex-end w-full mx-4 my-2 text-xl"><h3>Password:</h3></div>
+            <input class= "w-full rounded-full px-4 py-2 text-[#0d0f0f] text-xl" type="password" name = "password" value="<?php echo $password; ?>">
+            <span class= 'text-[#dc3545]'><?php echo $passwordErr?></span><br>
+            <input type="submit" value= "Submit" class="rounded-xl bg-[#808080] hover:bg-green-500 hover:text-white w-full px-2 py-1 mb-3">
+            <span>Still not registered?<a href="register.php" class="text-[#dc3545] hover:text-green-500"> Click Here!</a></span>
+        </form>
+        
+    </div>
+    
 </body>
 </html>
